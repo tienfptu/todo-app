b6bd14247ec3212fe5bc35c74f455a301756fadd
Cuoi <tiennvde140153@fpt.edu.vn>
2022-09-15 00:36:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const inputRef = useRef();
+
+    const [data, setData] = useState([]);
+
+    const [search, setSearch] = useState('');
+
+    const handleSubmit = () => {
+        setData([
+            ...data,
+            {
+                status: false,
+                name: inputRef.current.value,
+            },
+        ]);
+    };
+
+    const handleCheck = (e, index) => {
+        const newData = [...data];
+        newData[index].status = e.currentTarget.checked;
+        setData(newData);
+    };
+
+    return (
+        <div>
+            <h2>Todo list </h2>
+            <div>
+                <input ref={inputRef} />
+                <button onClick={handleSubmit}>Add</button>
+            </div>
+
+            <div>
+                <input placeholder="Enter keywords" onChange={(e) => setSearch(e.target.value)} />
+            </div>
+
+            <ul>
+                {data
+                    .filter((e) => e.name.includes(search))
+                    .map((e, index) => (
+                        <li>
+                            {e.status ? <strike>{e.name}</strike> : <span>{e.name}</span>}
+
+                            <input type="checkbox" checked={e.status} onChange={(ev) => handleCheck(ev, index)} />
+                        </li>
+                    ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
